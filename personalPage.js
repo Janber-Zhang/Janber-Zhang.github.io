a421b49e9246cecb80bf5520d23f4cb42b462689
janber <janber_zhang@163.com>
2016-06-29 00:25:39 +0800

    change the animation

--- personalPage.js
+++ personalPage.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 	// Personal Information
 	$("#personal_Inf_img").mouseenter(function(){
-		$("#personal_Inf_a").fadeIn();
+		$("#personal_Inf_a").slideDown();
 	});
 	$("#personal_Inf_img").mouseleave(function(){
-		$("#personal_Inf_a").fadeOut();
+		$("#personal_Inf_a").slideUp();
 	});
 	// repo list
 	$("#repo_list_img").mouseenter(function(){
-		$("#repo_list").fadeIn();
+		$("#repo_list").slideDown();
 	});
 	$("#repo_list_img").mouseleave(function(){
-		$("#repo_list").fadeOut();
+		$("#repo_list").slideUp();
 	});
 });
