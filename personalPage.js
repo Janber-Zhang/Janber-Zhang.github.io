1d6204f45b55496f30faa430e48020df0bf23ee5
janber <janber_zhang@163.com>
2016-06-29 21:46:26 +0800

    add media query and some personal information

--- personalPage.js
+++ personalPage.js
@@ -6,11 +6,32 @@ $(document).ready(function(){
 	$("#personal_Inf_img").mouseleave(function(){
 		$("#personal_Inf_a").slideUp();
 	});
-	// repo list
-	$("#repo_list_img").mouseenter(function(){
-		$("#repo_list").slideDown();
+	// repo list1
+	$("#repo_list_img1").mouseenter(function(){
+		$("#repo_list1").slideDown();
 	});
-	$("#repo_list_img").mouseleave(function(){
-		$("#repo_list").slideUp();
+	$("#repo_list_img1").mouseleave(function(){
+		$("#repo_list1").slideUp();
+	});
+	// repo list2
+	$("#repo_list_img2").mouseenter(function(){
+		$("#repo_list2").slideDown();
+	});
+	$("#repo_list_img2").mouseleave(function(){
+		$("#repo_list2").slideUp();
+	});
+	// repo list3
+	$("#repo_list_img3").mouseenter(function(){
+		$("#repo_list3").slideDown();
+	});
+	$("#repo_list_img3").mouseleave(function(){
+		$("#repo_list3").slideUp();
+	});
+	// repo list4
+	$("#repo_list_img4").mouseenter(function(){
+		$("#repo_list4").slideDown();
+	});
+	$("#repo_list_img4").mouseleave(function(){
+		$("#repo_list4").slideUp();
 	});
 });
