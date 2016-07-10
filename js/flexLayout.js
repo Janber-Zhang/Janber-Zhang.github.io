e5fbf85415b82ec33696ae210eeca96fbdb29c1a
janber <janber_zhang@163.com>
2016-07-10 20:17:31 +0800

    使用FlexBox重新布局

---
+++ js/flexLayout.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	// 个人信息
+	$("#personal_Inf_img").mouseover(function(){
+		$("#personal_Inf_a").slideDown();
+	});
+	$("#personal_Inf_img").mouseout(function(){
+		$("#personal_Inf_a").slideUp();
+	});
+
+	//更多信息
+	$(".repo_list_img").mouseover(function(){
+		$(this).css({"width":"50%","transform":"rotate(360deg)"});
+		$(this).next().slideDown();
+	});
+	$(".repo_list_img").mouseout(function(){
+		$(this).css({"width":"30%","transform":"rotate(0deg)"});
+		$(this).next().slideUp();
+	}); 
+})
