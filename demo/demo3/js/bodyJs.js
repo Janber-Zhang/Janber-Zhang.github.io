d4f6ddae33bf50f3e1bb6f93e2565c4d23ca58b2
janber <janber_zhang@163.com>
2016-07-31 01:52:07 +0800

    添加搜索结果显示为20本

--- demo/demo3/js/bodyJs.js
+++ demo/demo3/js/bodyJs.js
@@ -1,30 +1,81 @@
 $(document).ready(function(){
-	$('#toSearch').click(function() {
-		var keywords= $('#keyword_search').val();
+	function searchBook(){
+		var keywords= $('#keyword_search').val(); 
 		alert('是否查询：'+keywords);
 		$.ajax({
 	             type: "get",
 	             async: true,
-	             url: 'https://api.douban.com/v2/book/search?q='+ keywords +'&fields=id,title,image,summary',
+	             url: 'https://api.douban.com/v2/book/search?q='+ keywords +'&fields=id,title,image,summary,author,url',
 	             dataType: "jsonp",
 	             jsonp: "callback",//传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(一般默认为:callback)
 	             jsonpCallback:"aha",//自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名，也可以写"?"，jQuery会自动为你处理数据
 	             success: function(json){
-	             	alert('相关书籍有:' + json.total + '本');
+	             	$('.cell:first').show();
+	             	$('.cell:gt(0)').remove();
+	             	alert('查询成功！！！相关书籍有:' + json.total + '本');
 	                $('#bookTotal').text('相关书籍有:' + json.total + '本');
 	                // 显示书本信息
-	                $('.cell').find('img').attr({
-	                	src: json.books[0].image,
-	                	alt: json.books[0].title
-	                });
-	                $('.cell').find('h2').html(json.books[0].title)
-	                $('.cell').find('p').html(json.books[0].summary)
+	                for (var i = 0 ; i <= 19; i++) {
+	                	$('.cell:first').clone().appendTo('.wrap');
+	                	$('.cell:last').find('img').attr({
+	                	src: json.books[i].image,
+	                	alt: json.books[i].title
+		                });
+		                $('.cell:last').find('.bookTitle').html(json.books[i].title);
+		                $('.cell:last').find('.bookAuthor').html('作者：'+json.books[i].author);
+		                $('.cell:last').find('.bookSummary').html(json.books[i].summary);
+		                // $('.cell:last').find('a').attr('href', json.books[i].url);
+		                $('.cell:last').find('a').attr({
+		                	target: 'block',
+		                	href: json.books[i].url
+		                });
+	                }
+	                $('.cell:first').hide();
 	             },
 	             error: function(){
 	                alert('查询失败');
 	             }
-	         });
+	    });
+	};
+	$('#toSearch').click(searchBook);
+	$('#keyword_search').keypress(function(event) {
+		if (event.keyCode == "13"){
+			searchBook();
+		}
 	});
+	// $('#toSearch').click(function() {
+	// 	var keywords= $('#keyword_search').val(); 
+	// 	alert('是否查询：'+keywords);
+	// 	$.ajax({
+	//              type: "get",
+	//              async: true,
+	//              url: 'https://api.douban.com/v2/book/search?q='+ keywords +'&fields=id,title,image,summary,author',
+	//              dataType: "jsonp",
+	//              jsonp: "callback",//传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(一般默认为:callback)
+	//              jsonpCallback:"aha",//自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名，也可以写"?"，jQuery会自动为你处理数据
+	//              success: function(json){
+	//              	$('.cell:first').show();
+	//              	$('.cell:gt(0)').remove();
+	//              	alert('查询成功！！！相关书籍有:' + json.total + '本');
+	//                 $('#bookTotal').text('相关书籍有:' + json.total + '本');
+	//                 // 显示书本信息
+	//                 for (var i = 0 ; i <= 19; i++) {
+	//                 	$('.cell:first').clone().appendTo('.wrap');
+	//                 	$('.cell:last').find('img').attr({
+	//                 	src: json.books[i].image,
+	//                 	alt: json.books[i].title
+	// 	                });
+	// 	                $('.cell:last').find('.bookTitle').html(json.books[i].title);
+	// 	                $('.cell:last').find('.bookAuthor').html('作者：'+json.books[i].author);
+	// 	                $('.cell:last').find('.bookSummary').html(json.books[i].summary);
+	//                 }
+	//                 $('.cell:first').hide();
+	//              },
+	//              error: function(){
+	//                 alert('查询失败');
+	//              }
+	//     });
+	// });
 
 });
 
