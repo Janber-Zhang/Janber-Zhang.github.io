ccf317aaefd233085dbbcd72180920a5d6c1e4c2
janber <janber_zhang@163.com>
2016-08-01 23:47:10 +0800

    添加按评论数排序

--- demo/demo3/js/bodyJs.js
+++ demo/demo3/js/bodyJs.js
@@ -14,30 +14,44 @@ $(document).ready(function(){
 	             	$('.cell:first').show();
 	             	$('.cell:gt(0)').remove();
 	             	// alert('查询成功！！！相关书籍有:' + json.total + '本');
-	                $('#bookTotal').text('相关书籍有:' + json.total + '本');
-	                // 显示书本信息
-	                for (var i = 0 ; i <= json.count-1; i++) {
-	                	$('.cell:first').clone().appendTo('.wrap');
-	                	$('.cell:last').find('img').attr({
-	                	src: json.books[i].image,
-	                	alt: json.books[i].title
-		                });
-		                $('.cell:last').find('.bookTitle').html(json.books[i].title);
-		                $('.cell:last').find('.bookAuthor').html('作者：' + json.books[i].author +' / ' +json.books[i].publisher+ ' / ' + json.books[i].pubdate );
-		                if (json.books[i].summary) {
-		                	$('.cell:last').find('.bookSummary').html(json.books[i].summary);
-		                }else{
-		                	$('.cell:last').find('.bookSummary').html('暂无简介');
-		                }
-		                $('.cell:last').find('a').attr({
-		                	target: 'block',
-		                	href: json.books[i].url
-		                });
-		                var jdt_width = json.books[i].rating.average * 10;
-		                $('.cell:last').find('#jdt').css('width', jdt_width+'%');
-		                $('.cell:last').find('.raterNum').html('(' + json.books[i].rating.numRaters + '人评价）')
+                $('#bookTotal').text('相关书籍有:' + json.total + '本');
+                // 按评论数排序
+           			var bookSort = new Array();
+           			var bookBox = json.books;
+             		for (var i = 0; i < json.count; i++) {
+             			for (var j = 0; j < json.count - i -1; j++) {
+             				if (bookBox[j].rating.numRaters < bookBox[j + 1].rating.numRaters) {
+             					var temp = bookBox[j];
+             					bookBox[j] = bookBox[j + 1];
+             					bookBox[j+1] = temp;
+             				}
+             			}
+             		}
+             		bookSort = bookBox;
+             		
+                // 显示书本信息
+                for (var i = 0 ; i <= json.count-1; i++) {
+                	$('.cell:first').clone().appendTo('.wrap');
+                	$('.cell:last').find('img').attr({
+                	src: bookSort[i].image,
+                	alt: bookSort[i].title
+	                });
+	                $('.cell:last').find('.bookTitle').html(bookSort[i].title);
+	                $('.cell:last').find('.bookAuthor').html('作者：' + bookSort[i].author +' / ' +bookSort[i].publisher+ ' / ' + bookSort[i].pubdate );
+	                if (bookSort[i].summary) {
+	                	$('.cell:last').find('.bookSummary').html(bookSort[i].summary);
+	                }else{
+	                	$('.cell:last').find('.bookSummary').html('暂无简介');
 	                }
-	                $('.cell:first').hide();
+	                $('.cell:last').find('a').attr({
+	                	target: 'block',
+	                	href: bookSort[i].url
+	                });
+	                var jdt_width = bookSort[i].rating.average * 10;
+	                $('.cell:last').find('#jdt').css('width', jdt_width+'%');
+	                $('.cell:last').find('.raterNum').html('(' + bookSort[i].rating.numRaters + '人评价）')
+                }
+                $('.cell:first').hide();
 	             },
 	             error: function(){
 	                alert('查询失败');
@@ -53,39 +67,6 @@ $(document).ready(function(){
 			searchBook();
 		}
 	});
-	// $('#toSearch').click(function() {
-	// 	var keywords= $('#keyword_search').val(); 
-	// 	alert('是否查询：'+keywords);
-	// 	$.ajax({
-	//              type: "get",
-	//              async: true,
-	//              url: 'https://api.douban.com/v2/book/search?q='+ keywords +'&fields=id,title,image,summary,author',
-	//              dataType: "jsonp",
-	//              jsonp: "callback",//传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(一般默认为:callback)
-	//              jsonpCallback:"aha",//自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名，也可以写"?"，jQuery会自动为你处理数据
-	//              success: function(json){
-	//              	$('.cell:first').show();
-	//              	$('.cell:gt(0)').remove();
-	//              	alert('查询成功！！！相关书籍有:' + json.total + '本');
-	//                 $('#bookTotal').text('相关书籍有:' + json.total + '本');
-	//                 // 显示书本信息
-	//                 for (var i = 0 ; i <= 19; i++) {
-	//                 	$('.cell:first').clone().appendTo('.wrap');
-	//                 	$('.cell:last').find('img').attr({
-	//                 	src: json.books[i].image,
-	//                 	alt: json.books[i].title
-	// 	                });
-	// 	                $('.cell:last').find('.bookTitle').html(json.books[i].title);
-	// 	                $('.cell:last').find('.bookAuthor').html('作者：'+json.books[i].author);
-	// 	                $('.cell:last').find('.bookSummary').html(json.books[i].summary);
-	//                 }
-	//                 $('.cell:first').hide();
-	//              },
-	//              error: function(){
-	//                 alert('查询失败');
-	//              }
-	//     });
-	// });
+	
 
 });
-
