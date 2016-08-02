b1d68aca0d00de51fce92cebd09734909a6018aa
janber <janber_zhang@163.com>
2016-08-02 22:36:51 +0800

    并没有添加什么新内容

--- demo/demo3/js/bodyJs.js
+++ demo/demo3/js/bodyJs.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		$.ajax({
 	             type: "get",
 	             async: true,
-	             url: 'https://api.douban.com/v2/book/search?q='+ keywords +'&fields=all',
+	             url: 'https://api.douban.com/v2/book/search?q='+ keywords +'&fields=all&count=25',
 	             dataType: "jsonp",
 	             jsonp: "callback",
 	             jsonpCallback:"aha",
@@ -14,44 +14,45 @@ $(document).ready(function(){
 	             	$('.cell:first').show();
 	             	$('.cell:gt(0)').remove();
 	             	// alert('查询成功！！！相关书籍有:' + json.total + '本');
-                $('#bookTotal').text('相关书籍有:' + json.total + '本');
-                // 按评论数排序
-           			var bookSort = new Array();
-           			var bookBox = json.books;
-             		for (var i = 0; i < json.count; i++) {
-             			for (var j = 0; j < json.count - i -1; j++) {
-             				if (bookBox[j].rating.numRaters < bookBox[j + 1].rating.numRaters) {
-             					var temp = bookBox[j];
-             					bookBox[j] = bookBox[j + 1];
-             					bookBox[j+1] = temp;
-             				}
-             			}
-             		}
-             		bookSort = bookBox;
-             		
-                // 显示书本信息
-                for (var i = 0 ; i <= json.count-1; i++) {
-                	$('.cell:first').clone().appendTo('.wrap');
-                	$('.cell:last').find('img').attr({
-                	src: bookSort[i].image,
-                	alt: bookSort[i].title
-	                });
-	                $('.cell:last').find('.bookTitle').html(bookSort[i].title);
-	                $('.cell:last').find('.bookAuthor').html('作者：' + bookSort[i].author +' / ' +bookSort[i].publisher+ ' / ' + bookSort[i].pubdate );
-	                if (bookSort[i].summary) {
-	                	$('.cell:last').find('.bookSummary').html(bookSort[i].summary);
-	                }else{
-	                	$('.cell:last').find('.bookSummary').html('暂无简介');
+	                $('#bookTotal').text('相关书籍有:' + json.total + '本');
+	                // 按评论数排序
+	           			var bookSort = new Array();
+	           			var bookBox = json.books;
+	             		for (var i = 0; i < json.count; i++) {
+	             			for (var j = 0; j < json.count - i -1; j++) {
+	             				if (bookBox[j].rating.numRaters < bookBox[j + 1].rating.numRaters) {
+	             					var temp = bookBox[j];
+	             					bookBox[j] = bookBox[j + 1];
+	             					bookBox[j+1] = temp;
+	             				}
+	             			}
+	             		}
+	             		bookSort = bookBox;             		
+	                // 显示书本信息
+	                for (var i = 0 ; i <= json.count-1; i++) {
+	                	$('.cell:first').clone().appendTo('.bookCell');
+	                	// 获得书籍封面
+	                	$('.cell:last').find('img').attr({
+	                	src: bookSort[i].image,
+	                	alt: bookSort[i].title
+		                });
+		                // 获得书籍详细信息
+		                $('.cell:last').find('.bookTitle').html(bookSort[i].title);
+		                $('.cell:last').find('.bookAuthor').html('作者：' + bookSort[i].author +' / ' +bookSort[i].publisher+ ' / ' + bookSort[i].pubdate );
+		                if (bookSort[i].summary) {
+		                	$('.cell:last').find('.bookSummary').html(bookSort[i].summary);
+		                }else{
+		                	$('.cell:last').find('.bookSummary').html('暂无简介');
+		                }
+		                $('.cell:last').find('a').attr({
+		                	target: 'block',
+		                	href: bookSort[i].url
+		                });
+		                var jdt_width = bookSort[i].rating.average * 10;
+		                $('.cell:last').find('#jdt').css('width', jdt_width+'%');
+		                $('.cell:last').find('.raterNum').html('(' + bookSort[i].rating.numRaters + '人评价）')
 	                }
-	                $('.cell:last').find('a').attr({
-	                	target: 'block',
-	                	href: bookSort[i].url
-	                });
-	                var jdt_width = bookSort[i].rating.average * 10;
-	                $('.cell:last').find('#jdt').css('width', jdt_width+'%');
-	                $('.cell:last').find('.raterNum').html('(' + bookSort[i].rating.numRaters + '人评价）')
-                }
-                $('.cell:first').hide();
+	                $('.cell:first').hide();
 	             },
 	             error: function(){
 	                alert('查询失败');
